55a8eddbf71dbca8b118e6599323d83ff9d4d9e0
danbraj <danbraj@users.noreply.github.com>
2018-06-01 17:19:45 +0200

    Prepare test cases

--- numbering.test.js
+++ numbering.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
-
 const { prep, recog } = require('./numbering');
+const tCases = getTestCases();
+
 
 describe('Numbering module', function () {
 
@@ -19,69 +20,50 @@ describe('Numbering module', function () {
     });
 
     describe('Check "prep" function features', function () {
-        
-        const tCases = [
-            {
-                params: [ 'start', 'stop', 4 ],
-                idx: 67,
-                expected: 'start0067stop'
-            },
-            {
-                params: [ 'start', 'stop', 4 ],
-                idx: 128,
-                expected: 'start0128stop'
-            },
-            {
-                params: [ 'one', 'two', 2 ],
-                idx: 2000,
-                expected: 'one2000two'
-            },
-        ];
-
-        const prepTest = (tCase) => {
-            return () => {
-                const result = prep(tCase.params[0], tCase.params[1], tCase.params[2]);
-                expect(result.get(tCase.idx)).to.equal(tCase.expected);;
-            }
-        }
-    
-        tCases.forEach((tCase) => {
-            it('strings should be the same', prepTest(tCase));
+ 
+        tCases['prep-function'].forEach((tCase) => {
+            it(
+                'strings should be the same',
+                caseTest(tCase, prep(tCase.args[0], tCase.args[1], tCase.args[2]))
+            );
         });
 
     });
 
     describe('Check "recog" function features', function () {
-        
-        const tCases = [
-            {
-                param: 'directory/img076.svg',
-                idx: 24,
-                expected: 'directory/img024.svg'
-            },
-            {
-                param: 'directory/img03.png',
-                idx: 256,
-                expected: 'directory/img256.png'
-            },
-            {
-                param: 'numbering00012_small',
-                idx: 1967,
-                expected: 'numbering01967_small'
-            },
-        ];
-
-        const recogTest = (tCase) => {
-            return () => {
-                const result = recog(tCase.param);
-                expect(result.get(tCase.idx)).to.equal(tCase.expected);;
-            }
-        }
     
-        tCases.forEach((tCase) => {
-            it('strings should be the same', recogTest(tCase));
+        tCases['recog-function'].forEach((tCase) => {
+            it(
+                'strings should be the same',
+                caseTest(tCase, recog(tCase.arg))
+            );
         });
         
     });
 
 });
+
+function caseTest(testCase, nb) {
+
+    return () => { expect(nb.get(testCase.idx)).to.equal(testCase.expected); };
+};
+
+function getTestCases() {
+
+    return ({
+        "prep-function": [
+            { "args": [ "start", "stop", 4 ], "idx": 67, "expected": "start0067stop" },
+            { "args": [ "start", "stop", 4 ], "idx": 128, "expected": "start0128stop" },
+            { "args": [ "one", "two", 2 ], "idx": 2000, "expected": "one2000two" },
+            { "args": [ "begin", "end", 1 ], "idx": 10, "expected": "begin10end" },
+            { "args": [ "prefix", "postfix", 0 ], "idx": 20, "expected": "prefix20postfix" }
+        ],
+        "recog-function": [
+            { "arg": "directory/img076.svg", "idx": 24, "expected": "directory/img024.svg" },
+            { "arg": "directory/img03.png", "idx": 256, "expected": "directory/img256.png" },
+            { "arg": "numbering00012_small", "idx": 1967, "expected": "numbering01967_small" },
+            { "arg": "img03.png", "idx": 70, "expected": "img70.png" },
+            { "arg": "img01m", "idx": 130, "expected": "img130m" }
+        ]
+    });
+};
